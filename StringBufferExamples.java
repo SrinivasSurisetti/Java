2cf78efa7be1e4fa8abff2f8bbc9d64914a47a15
SrinivasSurisetti <119723614+SrinivasSurisetti@users.noreply.github.com>
2024-02-17 21:13:12 +0530

    Add files via upload

---
+++ StringBufferExamples.java
@@ -0,0 +1,9 @@
+public class StringBufferExamples {
+    public static void main(String[] args) {
+            //    String s = "Hello";
+            String p = "xyz";
+            String q = "xyzaaaaa";
+            System.out.println(q==p);
+   
+    }
+}
